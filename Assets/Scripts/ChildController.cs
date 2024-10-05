04b9c3c882e00e4a29c058ef76ebd2777305f5f7
Sava Minic <minic.sava@gmail.com>
2024-10-05 19:43:18 +0200

    jumping

--- Assets/Scripts/ChildController.cs
+++ Assets/Scripts/ChildController.cs
@@ -1,33 +1,34 @@
-using System;
 using UnityEngine;
 
 public class ChildController : MonoBehaviour
 {
-    [SerializeField]
-    private string horizontalAxis = "Horizontal";
-    
-    [SerializeField]
-    private string verticalAxis = "Vertical";
-    
-    [SerializeField]
-    private float speed;
-    
-    [SerializeField]
-    private float rotationSpeed;
-    
-    [SerializeField]
-    private Transform charTransform;
-    
-    [SerializeField]
-    private Rigidbody charBody;
-    
-    [SerializeField]
-    private Animator animator;
-    
+    private static readonly int IsWalkingAnimKey = Animator.StringToHash("isWalking");
+
+    [SerializeField] private string horizontalAxis = "Horizontal";
+
+    [SerializeField] private string verticalAxis = "Vertical";
+
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    [SerializeField] private LayerMask jumpLayerMask;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private float jumpForce = 2.5f;
+
+    [SerializeField] private Transform charTransform;
+
+    [SerializeField] private Rigidbody charBody;
+
+    [SerializeField] private Animator animator;
+
     private Camera _mainCamera;
 
     private float _rotation;
-    private static readonly int IsWalkingAnimKey = Animator.StringToHash("isWalking");
+    private bool isJumping;
+    private static readonly int JumpAnimKey = Animator.StringToHash("jump");
 
     private void Awake()
     {
@@ -36,15 +37,17 @@ public class ChildController : MonoBehaviour
 
     private void Update()
     {
-        var isKeyPressed = Input.GetAxis(horizontalAxis) != 0f || Input.GetAxis(verticalAxis) != 0f;
-        var inputVector = new Vector3(Input.GetAxis(horizontalAxis),0, Input.GetAxis(verticalAxis));
-        float cameraFacing = _mainCamera.transform.eulerAngles.y;
+        var isMoving = Input.GetAxis(horizontalAxis) != 0f || Input.GetAxis(verticalAxis) != 0f;
+
+        var inputVector = new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis));
+        var cameraFacing = _mainCamera.transform.eulerAngles.y;
         var moveVector = Quaternion.Euler(0, cameraFacing, 0) * inputVector;
 
         var velocity = moveVector * speed;
         charBody.velocity = new Vector3(velocity.x, charBody.velocity.y, velocity.z);
 
-        if (isKeyPressed)
+        // Rotation
+        if (isMoving)
         {
             var rotationStep = rotationSpeed * Time.deltaTime;
             var newDirection = Vector3.RotateTowards(transform.forward, moveVector.normalized, rotationStep, 0.0f);
@@ -54,7 +57,23 @@ public class ChildController : MonoBehaviour
         {
             charBody.angularVelocity = Vector3.zero;
         }
-        
-        animator.SetBool(IsWalkingAnimKey, isKeyPressed);
+
+        // Jumping
+        if (!isJumping && Input.GetKeyDown(jumpKey))
+        {
+            isJumping = true;
+            charBody.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            animator.SetTrigger(JumpAnimKey);
+        }
+
+        animator.SetBool(IsWalkingAnimKey, isMoving);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (((1 << collision.gameObject.layer) & jumpLayerMask) != 0)
+        {
+            isJumping = false;
+        }
     }
 }
