88ff678efc8286cc03fcaafcccd1099bdedd595e
Sava Minic <minic.sava@gmail.com>
2024-10-05 22:40:32 +0200

    added hp and crying

--- Assets/Scripts/ChildController.cs
+++ Assets/Scripts/ChildController.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class ChildController : MonoBehaviour
 {
     private static readonly int IsWalkingAnimKey = Animator.StringToHash("isWalking");
     private static readonly int JumpAnimKey = Animator.StringToHash("jump");
+    private static readonly int CryAnimKey = Animator.StringToHash("cry");
+    private static readonly int IsCryingAnimKey = Animator.StringToHash("isCrying");
 
     [SerializeField] private string horizontalAxis = "Horizontal";
 
@@ -27,6 +30,10 @@ public class ChildController : MonoBehaviour
 
     [SerializeField] private float jumpForce = 2.5f;
 
+    [SerializeField] private float maximumHitPoints = 100;
+
+    [SerializeField] private float cryDelay = 3f;
+
     [SerializeField] private Vector2 throwForce;
 
     [SerializeField] private Transform charTransform;
@@ -36,36 +43,45 @@ public class ChildController : MonoBehaviour
     [SerializeField] private Animator animator;
 
     private GrabController _grabController;
-    private ThrowableObject _throwableObject;
-    private ThrowableObject _thrownObject;
-    private float _invulnerableTime;
     private HitFeedback _hitFeedback;
+
+    private float _hitPoints;
+    private float _invulnerableTime;
+    private bool _isJumping;
+    private IEnumerator _cryAnimation;
 
     private Camera _mainCamera;
 
     private float _rotation;
-    private bool _isJumping;
+    private ThrowableObject _throwableObject;
+    private ThrowableObject _thrownObject;
+
+    private bool IsAlive => _hitPoints > 0;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
         _grabController = GetComponentInChildren<GrabController>();
         _hitFeedback = GetComponentInChildren<HitFeedback>();
+
+        _hitPoints = maximumHitPoints;
     }
 
     private void Update()
     {
-        var isMoving = Input.GetAxis(horizontalAxis) != 0f || Input.GetAxis(verticalAxis) != 0f;
-
         var inputVector = new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis));
         var cameraFacing = _mainCamera.transform.eulerAngles.y;
         var moveVector = Quaternion.Euler(0, cameraFacing, 0) * inputVector;
 
-        var velocity = moveVector * speed;
-        charBody.velocity = new Vector3(velocity.x, charBody.velocity.y, velocity.z);
+        if (IsAlive)
+        {
+            var velocity = moveVector * speed;
+            charBody.velocity = new Vector3(velocity.x, charBody.velocity.y, velocity.z);
+        }
 
         // Rotation
-        if (isMoving)
+        var isMoving = Input.GetAxis(horizontalAxis) != 0f || Input.GetAxis(verticalAxis) != 0f;
+        if (IsAlive && isMoving)
         {
             var rotationStep = rotationSpeed * Time.deltaTime;
             var newDirection = Vector3.RotateTowards(transform.forward, moveVector.normalized, rotationStep, 0.0f);
@@ -77,7 +93,7 @@ public class ChildController : MonoBehaviour
         }
 
         // Jumping
-        if (!_isJumping && Input.GetKeyDown(jumpKey))
+        if (IsAlive && !_isJumping && Input.GetKeyDown(jumpKey))
         {
             _isJumping = true;
             charBody.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
@@ -85,7 +101,7 @@ public class ChildController : MonoBehaviour
         }
 
         // hold/throw
-        if (_throwableObject == null && Input.GetKeyDown(holdThrowKey))
+        if (IsAlive && _throwableObject == null && Input.GetKeyDown(holdThrowKey))
         {
             if (_grabController.LatestGrabObject != null)
             {
@@ -95,18 +111,16 @@ public class ChildController : MonoBehaviour
                 _throwableObject.Hold(transform);
             }
         }
-        else if (_throwableObject != null && Input.GetKeyUp(holdThrowKey))
+        else if (_throwableObject != null
+                 && (Input.GetKeyUp(holdThrowKey) || !IsAlive))
         {
-            if (_throwableObject != null)
-            {
-                // throw it away
-                var throwVector = throwForce.x * transform.forward;
-                throwVector.y = throwForce.y;
-                _throwableObject.Throw(throwVector);
+            // throw it away
+            var throwVector = throwForce.x * transform.forward;
+            throwVector.y = throwForce.y;
+            _throwableObject.Throw(throwVector);
 
-                _thrownObject = _throwableObject;
-                _throwableObject = null;
-            }
+            _thrownObject = _throwableObject;
+            _throwableObject = null;
         }
 
         animator.SetBool(IsWalkingAnimKey, isMoving);
@@ -114,13 +128,10 @@ public class ChildController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (((1 << collision.gameObject.layer) & jumpLayerMask) != 0)
-        {
-            _isJumping = false;
-        }
-        
+        if (((1 << collision.gameObject.layer) & jumpLayerMask) != 0) _isJumping = false;
+
         // check for hit
-        if (Time.time > _invulnerableTime && ((1 << collision.gameObject.layer) & hitLayerMask) != 0)
+        if (IsAlive && Time.time > _invulnerableTime && ((1 << collision.gameObject.layer) & hitLayerMask) != 0)
         {
             var currentThrowable = collision.gameObject.GetComponent<ThrowableObject>();
             if (currentThrowable != null && currentThrowable != _thrownObject)
@@ -129,11 +140,32 @@ public class ChildController : MonoBehaviour
                 var damage = hitMagnitudeToDamage.Evaluate(magnitude);
                 if (damage > 0.1f)
                 {
-                    Debug.LogError($"{gameObject.name} COLLIDE {currentThrowable.name} v:{currentThrowable.VelocityMagnitude} d:{damage}");
                     _invulnerableTime = Time.time + invulnerableDelay;
                     _hitFeedback.DoHitAnimation();
+
+                    _hitPoints -= damage;
+                    if (_hitPoints <= 0)
+                    {
+                        _hitPoints = 0;
+                        if (_cryAnimation != null) StopCoroutine(_cryAnimation);
+                        StartCoroutine(_cryAnimation = CryBabyCry());
+                    }
+                    
+                    //Debug.LogWarning($"{gameObject.name} hit {currentThrowable.name}  d:{damage} hp:{_hitPoints}");
                 }
             }
         }
+    }
+
+    private IEnumerator CryBabyCry()
+    {
+        animator.SetBool(IsCryingAnimKey, true);
+        animator.SetTrigger(CryAnimKey);
+        _hitFeedback.ToggleCrying(true);
+        
+        yield return new WaitForSeconds(cryDelay);
+        _hitPoints = maximumHitPoints;
+        animator.SetBool(IsCryingAnimKey, false);
+        _hitFeedback.ToggleCrying(false);
     }
 }
--- Assets/Scripts/HitFeedback.cs
+++ Assets/Scripts/HitFeedback.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class HitFeedback : MonoBehaviour
 {
     [SerializeField] private Color hitColor;
+    [SerializeField] private Color cryColor;
     [SerializeField] private float hitDuration = 1f;
     [SerializeField] private AnimationCurve hitColorAnimationCurve;
     private IEnumerator _hitAnimation;
@@ -21,15 +22,22 @@ public class HitFeedback : MonoBehaviour
         StartCoroutine(_hitAnimation = DoHit());
     }
 
+    public void ToggleCrying(bool isCrying)
+    {
+        if (_hitAnimation != null) StopCoroutine(_hitAnimation);
+        _meshRenderer.material.color = isCrying ? cryColor : Color.white;
+    }
+
     private IEnumerator DoHit()
     {
         var time = 0f;
         while (time <= hitDuration)
         {
             var colorIntensity = hitColorAnimationCurve.Evaluate(time / hitDuration);
-            _meshRenderer.material.color = Color.Lerp(Color.white, Color.red, colorIntensity);
+            _meshRenderer.material.color = Color.Lerp(Color.white, hitColor, colorIntensity);
             time += Time.deltaTime;
             yield return null;
         }
     }
+    
 }
