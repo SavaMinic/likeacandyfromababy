7903ef60ab310df986843fb4392a2e3b5c76cf27
Sava Minic <minic.sava@gmail.com>
2024-10-06 10:06:30 +0200

    added candy

---
+++ Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject candyPrefab;
+    [SerializeField] private int initialCandyCount = 10;
+
+    [SerializeField] private float areaDistance = 7f;
+    [SerializeField] private float minHeight = 0.5f;
+    [SerializeField] private float maxHeight = 1.2f;
+
+    private void Start()
+    {
+        SpawnCandy(initialCandyCount);
+    }
+
+    private void SpawnCandy(int count)
+    {
+        // one is always at 0,0,0
+        Instantiate(candyPrefab, Vector3.up, Random.rotation, transform);
+        
+        // others are random
+        for (var i = 1; i < count; i++)
+        {
+            var spawnedCandy = Instantiate(candyPrefab, transform);
+            spawnedCandy.transform.position = new Vector3(
+                Random.Range(-areaDistance, areaDistance),
+                Random.Range(minHeight, maxHeight),
+                Random.Range(-areaDistance, areaDistance)
+            );
+            spawnedCandy.transform.rotation = Random.rotation;
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            // TODO: show menu instead of reload
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
